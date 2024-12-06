913c6bbbabad6982c9fa645d4c9d9994934ee252
Muhammad Usman <usman.aamir404@gmail.com>
2024-12-07 00:37:47 +0500

    Slider Component added

--- src/app/page.js
+++ src/app/page.js
@@ -3,13 +3,16 @@ import Slider from "@/components/image-slider/slider";
 import Image from "next/image";
 export default function Home() {
   return (
-    <div className="container mx-auto">
-      <main className="w-full p-4 flex gap-4 flex-col items-center justify-center">
-        <p>Welcome to Next Project</p>
-        <Slider className="p-4" 
-        images={["/image1.png", "/image2.png","/image3.png"]}
-        />
-        <div><Image src="/image1.png" width={200} height={200} alt="haah" /></div>
+    <div className="">
+      <main>
+        <div className="w-full bg-slate-400 p-4 flex gap-4 items-center justify-center">
+          <Slider className="p-4" 
+          />
+          <div>
+            <p className="text-3xl font-serif font-semibold">Welcome to Next Project</p>
+          </div>
+          {/* <div><Image src="/image1.png" width={200} height={200} alt="haah" /></div> */}
+        </div>
       </main>
     </div>
   );
