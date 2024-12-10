47aa111e614df5e95a5e39279e67245f1da699e6
Muhammad Usman <usman.aamir404@gmail.com>
2024-12-11 01:01:17 +0500

    code updated

--- src/app/page.js
+++ src/app/page.js
@@ -1,20 +1,20 @@
 import React from "react";
-import Slider from "@/components/image-slider/slider";
+import Slider from "@/components/slider";
 // import Image from "next/image";
 import Link from "next/link";
 export default function Home() {
   return (
     <div className="">
       <main>
-        <div className="w-full bg-slate-400 p-4 flex flex-col-reverse lg:flex-row gap-4 items-center ">
+        <div className="w-full bg-slate-400 p-4 flex flex-col-reverse lg:flex-row gap-4 items-center justify-center">
           <Slider className="p-4" 
           />
-          <div className="flex flex-col items-center">
-            <h1 className="text-3xl font-serif font-semibold">Next Level Food for NextLevel Foodies</h1>
+          <div className="flex flex-col items-center justify-center text-center p-4">
+            <h1 className="text-6xl font-serif font-semibold">Next Level Food for NextLevel Foodies</h1>
             <p className="text-3xl font-serif font-semibold">Taste & share food from all over the world.</p>
             <div className="flex flex-col md:flex-row gap-3">
-              <Link href="/meals" className="bg-slate-500 text-white p-2 rounded-md">Order Now</Link>
-              <Link href="/community" className="bg-slate-500 text-white p-2 rounded-md">Join Community</Link>  
+              <Link href="/meals" className="text-gradient-to-r from-orange-500 to-orange-400 p-2 rounded-md">Order Now</Link>
+              <Link href="/community" className="bg-gradient-to-r from-pink-500 to-orange-500 text-white p-2 rounded-md">Join Community</Link>  
             
             </div>
           </div>
